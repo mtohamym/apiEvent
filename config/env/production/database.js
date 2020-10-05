7a4fe8afeff63df15a1c1f8edcf79be78763addb
Mohammed M El-tohamy <49570454+mtohamym@users.noreply.github.com>
2020-10-05 04:13:34 +0200

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
       default: {
         connector: 'mongoose',
         settings: {
-          uri: env('DATABASE_URI'),
+          uri: env('mongodb+srv://tohamymedo11:<01002693515++++++2m>@cluster0.8b9se.mongodb.net/<tohamymedo11>?retryWrites=true&w=majority'),
         },
         options: {
           ssl: true,
